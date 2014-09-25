8dfc666b47b60b245864c5a37a71b38411c02a10
32Del <vng150395@gmail.com>
2014-09-25 13:49:13 +0200

    Changement du caption de la form

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -186,7 +186,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "FrmBancomat";
-            this.Text = "Form1";
+            this.Text = "Bancomat";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
